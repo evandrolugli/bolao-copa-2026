49e5e1bb8d2586f5252abaf53d4380c6257a1a43
Evandro Lugli <78626020+evandrolugli@users.noreply.github.com>
2026-05-26 16:41:20 +1000

    fix header min-h-screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,12 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className="bg-zinc-950 text-white">
-				<Header />
-				<main className="max-w-5xl mx-auto p-4">{children}</main>
+			<body className="min-h-screen bg-zinc-100 text-zinc-900 antialiased">
+				<div className="flex min-h-screen flex-col">
+					<Header />
+
+					<main className="flex-1">{children}</main>
+				</div>
 			</body>
 		</html>
 	);
